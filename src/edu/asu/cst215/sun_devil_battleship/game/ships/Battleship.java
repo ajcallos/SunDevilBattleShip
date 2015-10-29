9d8b9260a0bda4d29642531ffd0cb80c7d20a5b5
KuraudoK <alexander.callos@gmail.com>
2015-10-29 15:13:31 -0700

    Battleship class updated
    The first warship, the Battleship was completed.

--- src/edu/asu/cst215/sun_devil_battleship/game/ships/Battleship.java
+++ src/edu/asu/cst215/sun_devil_battleship/game/ships/Battleship.java
@@ -16,10 +16,18 @@ public class Battleship implements Warship
     private final Facing facing;
     private final Vector2d location;
 
+    private boolean sunk = false;
+    private int[] damage = new int[shipLength];
+
     public Battleship(Vector2d location, Facing facing)
     {
         this.location = location;
         this.facing = facing;
+
+        for (int i = 0; i < shipLength; i++)
+        {
+            damage[i] = 0;
+        }
     }
 
     @Override
@@ -41,6 +49,11 @@ public class Battleship implements Warship
     }
 
     @Override
+    public Vector2d getLocation()
+    {
+        return location;
+    }
+    @Override
     public Facing getFacing()
     {
         return this.facing;
@@ -49,13 +62,31 @@ public class Battleship implements Warship
     @Override
     public boolean isSunk()
     {
-        return false;
+        return sunk;
     }
 
     @Override
-    public boolean damage(Vector2d shot)
+    public boolean damage(int location)
     {
-        return false;
+        if (damage[location] == 1)
+            return false;
+        else
+            damage[location] = 1;
+
+        if (checkSunk())
+            return true;
+        else
+            return false;
     }
-    
+
+    private boolean checkSunk()
+    {
+        for (int i = 0; i < shipLength; i++)
+        {
+            if (damage[i] == 0)
+                return false;
+        }
+
+        return true;
+    }
 }
